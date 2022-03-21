41b1a634a891a1fc5a635c6351e43f292eca852c
Michael Zhang <mzzhang2014@gmail.com>
2022-03-21 03:46:24 -0700

    Added some instructions to pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#ex: ./pipeline.sh ERS_NGTS10_2022_seg_???.fits
 for filename in "$@"
 do
     echo $filename
@@ -7,3 +8,8 @@ do
     python sub_bkd.py rateints_$filename
     python optimal_extract.py bkdsub_rateints_$filename
 done
+
+#To do phase curve fitting:
+#python extract_phase_curve.py ngts_10.cfg start_bin end_bin
+
+#To do phase curve fitting while fixing transit and eclipse params, see run_all_wavelengths.sh
