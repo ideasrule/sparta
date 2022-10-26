bd4de093a208f807ca5b7c12a5ce143cc95cb87b
Michael Zhang <zmzhang@uchicago.edu>
2022-10-26 14:54:26 -0500

    Parallelism in run_all_wavelengths (not race-condition immune

--- run_all_wavelengths.sh
+++ run_all_wavelengths.sh
@@ -4,6 +4,8 @@ rm result.txt lightcurves.txt
 for i in {5000..11800..500}
 do
     echo $i $(($i+500))
-    python -u ../miri_pipeline/extract_phase_curve_limited.py gj1214.cfg $i $(($i+500)) --burn-in-runs 2000 --production-runs 1000 -b 64
+    python -u ../miri_pipeline/extract_phase_curve_limited.py gj1214.cfg $i $(($i+500)) --burn-in-runs 10000 --production-runs 10000 -b 1 --extra-phase-terms &
     #python -u ../miri_pipeline/extract_transit_limited.py toi134.cfg $i $(($i+200)) --burn-in-runs 2000 --production-runs 1000 -b 16
 done
+
+wait
