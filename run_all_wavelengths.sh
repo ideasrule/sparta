f969a55328be5b9ff3fa48a2f33c81663ac4215c
Michael Zhang <mzzhang2014@gmail.com>
2024-01-24 13:33:44 -0600

    Updating documentation for latest code changes

--- run_all_wavelengths.sh
+++ run_all_wavelengths.sh
@@ -4,8 +4,14 @@ rm result.txt lightcurves.txt
 for i in {5000..11800..500}
 do
     echo $i $(($i+500))
-    python -u ../miri_pipeline/extract_phase_curve_limited.py gj1214.cfg $i $(($i+500)) --burn-in-runs 5000 --production-runs 5000 -b 1 --extra-phase-terms  &
+    python -u ../miri_pipeline/extract_phase_curve_limited.py gj1214.cfg $i $(($i+500)) --burn-in-runs 2000 --production-runs 1000 -b 1 --extra-phase-terms -e 525 &
     #python -u ../miri_pipeline/extract_transit_limited.py toi134.cfg $i $(($i+200)) --burn-in-runs 2000 --production-runs 1000 -b 16
 done
 
 wait
+
+
+sort -gk1 result.txt > temp
+mv temp result.txt
+sort -gk1 lightcurves.txt > temp
+mv temp lightcurves.txt
